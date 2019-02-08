62124ddd4bf423d43e1e55829fdcb1d6219c12d8
Robyn Graham <robyngraham@zipcoders-MacBook-Pro.local>
2019-02-08 12:26:06 -0500

    Finished SumofInput

--- Main.java
+++ Main.java
@@ -1,9 +1,46 @@
+import java.util.Scanner;
 /**
- * Created by iyasuwatts on 10/17/17.
+ * Created by iyasuwatts on 10/17/17
+ * String question Write question for user to be asked
+ * System.out.println Ask user to input a number
+ * scanner Get user input
+ * count beginning from until userInputValue Calculate the sum of user input
+ * System.out.println(sum);Display the sum 
  */
 public class Main {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args){  
+       String question = "Enter a number";
+       System.out.println(question);
+      
+       Scanner scanner = new Scanner(System.in);
+       int userInputValue = scanner.nextInt();
+       
+       int sum =0;
+       
+           for (int i = 0; i<=userInputValue; i++){
+               sum = sum + i;
+               
+  
+            
+        }
+        
+        System.out.println(sum);
+         
+        }
+        
+               
+               
+        
+       
+       
     }
-}
+
+        
+        
+    
+        
+            
+        
+    
+
